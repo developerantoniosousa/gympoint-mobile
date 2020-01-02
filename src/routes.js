a0fc5e7f212bd9cc0905a63682b2a939c32064e1
Antonio Sousa <developerantoniosousa@gmail.com>
2020-01-02 02:14:59 -0300

    created router with screens

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import SignIn from '~/screens/SignIn';
+import Checkins from '~/screens/Checkins';
+import AnswerList from '~/screens/AnswerList';
+import Answer from '~/screens/Answer';
+import Question from '~/screens/Question';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    User: createBottomTabNavigator({
+      Checkins,
+      Answers: createStackNavigator({
+        AnswerList,
+        Answer,
+        Question,
+      }),
+    }),
+  }),
+);
